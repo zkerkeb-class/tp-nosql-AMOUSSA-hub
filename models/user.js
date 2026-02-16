3ab02f754f8e740af17d2f52188f09cd1a323ce5
[dyad] <amoussa.rayan77130@hotmail.fr>
2026-02-16 23:24:00 +0100

    [dyad] Correction de l'erreur 'next is not a function' dans le middleware pre-save du modèle User. - wrote 1 file(s)

--- models/user.js
+++ models/user.js
@@ -14,14 +14,22 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// hasher le mot de passe avant de l'enregistrer
-userSchema.pre('save', async function (next) {
+// Middleware pre-save pour hasher le mot de passe avant de l'enregistrer
+userSchema.pre('save', function (next) { // Retiré 'async' ici
     if (!this.isModified('password')) {
         return next();
     }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+    const user = this; // Capture le contexte 'this'
+    bcrypt.genSalt(10)
+        .then(salt => bcrypt.hash(user.password, salt))
+        .then(hash => {
+            user.password = hash;
+            next(); // Appelle next() après le hachage réussi
+        })
+        .catch(err => {
+            console.error('Erreur lors du hachage du mot de passe :', err);
+            next(err); // Passe l'erreur à Mongoose
+        });
 });
 
 // Méthode pour comparer les mots de passe
