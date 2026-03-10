46d13ef0bc32d9ea6d5776e31508dd627097f3aa
[dyad] <git@dyad.sh>
2026-03-10 15:58:25 +0100

    Remove 1 file

--- explanation.js
+++
@@ -1,8 +0,0 @@
-const team = await Team.findById(req.params.id)
-            .populate({
-                path: 'pokemons',
-                model: 'Pokemon',
-                localField: 'pokemons',
-                foreignField: 'id'
-            })
-            .populate('user', 'username');
